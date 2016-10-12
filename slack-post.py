cbec8a935344aa21a12741eeb4afd56150fa8cb2
Thuan Nguyen <ninhthuan82us@gmail.com>
2016-10-12 18:17:01 +0700

    Update slack-post.py

--- slack-post.py
+++ slack-post.py
@@ -3,11 +3,12 @@ import sys
  
 total = len(sys.argv)
 if total == 1:
-	print 'Usage: ./slack-post.py "Hello World"'
+	print 'Usage: ./slack-post.py "Hello World" "#channel"'
 	sys.exit()
 
 msg = sys.argv[1] # Message
+channel = sys.argv[2] # Channel
 
-webhook = 'https://hooks.slack.com/services/webhook-secret-url'
+webhook = 'https://hooks.slack.com/services/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'
 slack = slackweb.Slack(url=webhook)
-slack.notify(text=msg, channel="#bla-bla", username="8bit-bot", icon_emoji=":monkey_face:")
+slack.notify(text=msg, channel=channel, username="8bit-bot", icon_emoji=":monkey_face:")
