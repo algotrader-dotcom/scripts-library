40bfaf250886796317369f613c5e9b85349da696
Thuan Nguyen <ninhthuan82us@gmail.com>
2017-04-11 11:13:01 +0700

    Create elasticdump_backup.sh

---
+++ elasticdump_backup.sh
@@ -0,0 +1,27 @@
+# apt-get install nodejs && ln -s /usr/bin/nodejs /usr/bin/node && npm install elasticdump -g
+es_date=`date +%Y-%m-%d-%H-%M-%S`
+mkdir -p "/root/backup/"$es_date
+
+for index_name in index1 index2 index3
+do
+	#index_name="received_email"
+	es_src="http://172.31.15.125:9200/"$index_name
+	es_backup_mapping="/root/backup/"$es_date"/"$index_name"_mapping.json"
+	es_backup_data="/root/backup/"$es_date"/"$index_name"_data.json"
+	es_backup_analyzer="/root/backup/"$es_date"/"$index_name"_analyzer.json"
+
+	elasticdump \
+          --input=$es_src \
+          --output=$es_backup_analyzer \
+          --type=analyzer
+
+	elasticdump \
+	  --input=$es_src \
+	  --output=$es_backup_mapping \
+	  --type=mapping
+
+	elasticdump \
+	  --input=$es_src \
+	  --output=$es_backup_data \
+	  --type=data
+done
